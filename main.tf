e379dbcf3549cb877885fcd30ea8c4d145887a1b
avicii-devops <mydadisalive@gmail.com>
2023-07-28 22:53:08 +0300

    works

--- main.tf
+++ main.tf
@@ -1,5 +1,3 @@
-# main.tf
-
 provider "aws" {
   region = "us-east-1"  # Set your desired AWS region here
 }
@@ -8,10 +6,16 @@ resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "default" {
-  vpc_id              = aws_vpc.default.id
-  cidr_block          = "10.0.1.0/24"
-  availability_zone   = "us-east-1a"
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 }
 
 resource "aws_security_group" "instance_sg" {
@@ -33,25 +37,44 @@ resource "aws_security_group" "instance_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-# Read the public key file and store it in a variable
-locals {
-  public_key = file("mykey.pub")
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.default.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
 # Create an AWS key pair using the public key
 resource "aws_key_pair" "mykey" {
   key_name   = "mykey"
-  public_key = local.public_key
+  public_key = file("mykey.pub")
 }
 
 resource "aws_instance" "test_instance" {
   ami           = "ami-05548f9cecf47b442"  # Set to the Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.default.id  # Use the default subnet in the default VPC
+  subnet_id     = aws_subnet.public.id  # Use the public subnet for testing instance
   associate_public_ip_address = true  # Request a public IP address for the instance
-  private_ip     = "10.0.1.10"  # Specify the private IP address for the instance
 
   user_data = <<-EOT
     #!/bin/bash
@@ -75,9 +98,9 @@ resource "aws_instance" "test_instance" {
 resource "aws_instance" "prod_instance" {
   ami           = "ami-05548f9cecf47b442"  # Set to the Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.default.id  # Use the default subnet in the default VPC
+  subnet_id     = aws_subnet.public.id  # Use the private subnet for production instance
   associate_public_ip_address = true  # Request a public IP address for the instance
-  private_ip     = "10.0.1.11"  # Specify the private IP address for the instance
+
 
   user_data = <<-EOT
     #!/bin/bash
@@ -106,10 +129,21 @@ output "test_instance_private_ip" {
   value = aws_instance.test_instance.private_ip
 }
 
+output "prod_instance_private_ip" {
+  value = aws_instance.prod_instance.private_ip
+}
+
 output "prod_instance_public_ip" {
   value = aws_instance.prod_instance.public_ip
 }
 
-output "prod_instance_private_ip" {
-  value = aws_instance.prod_instance.private_ip
+# Output the public IPs to a txt file
+resource "local_file" "public_ips" {
+  filename = "public_ips.txt"
+  content  = <<EOT
+Test Instance Public IP: ${aws_instance.test_instance.public_ip}
+Prod Instance Private IP: ${aws_instance.prod_instance.private_ip}
+Public Subnet ID: ${aws_subnet.public.id}
+Private Subnet ID: ${aws_subnet.private.id}
+EOT
 }
